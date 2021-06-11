78cc1a7490e7f0ff1d4c8d1b089b7589edb7b616
James Smith <james.smith2@ndorms.ox.ac.uk>
2021-06-11 11:53:00 +0100

    Add EUCTR design extraction

--- scripts/automated_extraction.R
+++ scripts/automated_extraction.R
@@ -1,19 +1,17 @@
 require(tidyverse)
 
-# Ictrp_im847 ----- 
-# Repeat for new data
+# Combine datasets -----
+main <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/ictrp_main847.csv")
+im <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/ictrp_im847.csv")
+d <- bind_rows(main, im, .id = "id")
+d$id[d$id == 1] <- "main"
+d$id[d$id == 2] <- "im"
 
-d <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/covid847.csv")
-
-ct <- d %>% 
-  filter(Source_registry == "CT.gov")
-
-design <- ct %>% 
-  select(TrialID, Study_design)
+# TRIAL DESIGN -----
 
 # these are the outputs we need:
-# Use of control arm # if randomised, we assume there was a control arm (not implmented yet)
-# Randomisation  # if single arm, we assume not randomised
+# Use of control arm # if randomised, there was a control arm
+# Randomisation  # if single arm, not randomised
 # Blinding
 # Subject blind*
 # Caregiver blind*
@@ -22,7 +20,25 @@ design <- ct %>%
 # Analyst blind*
 # Primary purpose
 
-# this works and doesn't require all the messing around separating. Much easier
+# * if at least one of the specific parties blinded is stated, the others will be
+# recorded as “No”, unless it is clear that at least one other party has been
+# blinded (e.g. if the trial is described as double-blind but only one specific
+# party is described as blinded).In the latter case, the other parties will be
+# reported as unrecorded.If none are stated, they will all be recorded as
+# unreported
+
+# Clinicaltrials.gov ----- 
+
+# works for all three datasets
+
+# d <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/ictrp_main847.csv")
+
+ct <- d %>% 
+  filter(Source_registry == "CT.gov")
+
+design <- ct %>% 
+  select(TrialID, Study_design)
+
 design <- design %>% 
   mutate(
     # "Sequential Assignment" is variable in meaning so we don't assign that
@@ -60,9 +76,68 @@ design <- design %>%
       TRUE ~ "Other")
     )
 
-filter(ct, TrialID == "NCT02723812") %>% 
-  View()
+# Euctr -----
 
+eu <- d %>% 
+  filter(Source_registry == "EUCTR")
+
+design <- eu %>% 
+  select(TrialID, Study_design)
+
+design %>%
+  mutate(
+    # all in main or im are either controlled:yes or no
+    control_arm = case_when(
+      grepl("Controlled: yes", Study_design) ~ "Yes",
+      grepl("Controlled: no", Study_design) ~ "No"),
+    # all in main or im randomised:yes or no apart from one which is not controlled
+    # may need to add an unreported for other arms
+    randomisation = case_when(
+      control_arm == "No" ~ "No",
+      grepl("Randomised: no", Study_design) ~ "No",
+      grepl("Randomised: yes", Study_design) ~ "Yes",
+      grepl("Randomised:<br>", Study_design) ~ "Unreported"),
+    # all in main or im meet these
+    blinding = case_when(
+      grepl("blind: yes", Study_design) ~ "Yes", 
+      grepl("blind: no", Study_design) ~ "No", 
+      grepl("blind:<br>", Study_design) ~ "Unreported"),
+    # who specifically is blinded is not stated
+    # many say double or single but not who 
+    subject_blind = "Unreported", 
+    caregiver_blind = "Unreported",
+    investigator_blind = "Unreported",
+    outcome_blind = "Unreported",
+    analyst_blind = "Unreported",
+    primary_purpose = "Unreported"
+    )
+
+# check each item captures all trials. Done for im and main
+# all contain "blind"
+i <- grep("Randomised: yes|Randomised: no", 
+            design$Study_design)
+
+design$Study_design[!i]
+
+
+# Phase -----
+# data exploration 
+main <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/ictrp_main847.csv")
+im <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/ictrp_im847.csv")
+covid <- read_csv("data/manual_processing/eligibility_screen/eligible_trials/covid847.csv")
+
+table(main$Source_registry)
+table(im$Source_registry)
+table(covid$Source_registry)
+
+p <- c(unique(main$Phase), unique(im$phase), unique(covid$Phase))
+p <- unique(p)
+which(is.na(main$Phase))
+which(is.na(im$Phase))
+which(is.na(covid$Phase))
+
+
+# christiana to do countries
 
 # Notes -----
 
